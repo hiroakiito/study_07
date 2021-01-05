320d44b37f28acc8081ee3fedf918189c26e241f
hiroakiitou <pigaru.gooood.mar19@gmail.com>
2021-01-05 23:33:23 +0900

    余計なコード削除

--- translate.py
+++ translate.py
@@ -4,9 +4,7 @@ import eel
 import sys
 from googletrans import Translator
 
-def translate_ja(source, origin_lang, trans_lang):
-    print("元文は:{}".format(source))
+def translate_execute(source, origin_lang, trans_lang):
     translator = Translator(service_urls=['translate.googleapis.com'])
-    print('翻訳結果---->',translator.translate(source, src="{}".format(origin_lang) ,dest="{}".format(trans_lang)).text)
     result_text = (translator.translate(source, src="{}".format(origin_lang) ,dest="{}".format(trans_lang)).text)
-    eel.set_ja(result_text)
+    eel.set_trans_result(result_text)
--- view.py
+++ view.py
@@ -6,21 +6,9 @@ app_name="html"
 end_point="index.html"
 size=(700,600)
 
-# @ eel.expose
-# def order_input(order_num):
-#     search.order_input(order_num)
-
-# @ eel.expose
-# def calc(item_code, price, item_name ,amount):
-#     search.calc(item_code, price, item_name ,amount)
-
-# @ eel.expose
-# def make_recept(item_code, price, item_name ,amount, total_price, pay):
-#     search.make_recept(item_code, price, item_name ,amount, total_price, pay)
-
 @ eel.expose
-def translate_ja(source, origin_lang, trans_lang):
-    translate.translate_ja(source, origin_lang, trans_lang)
+def translate_execute(source, origin_lang, trans_lang):
+    translate.translate_execute(source, origin_lang, trans_lang)
  
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
